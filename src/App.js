2342fdf4384971dbc176049edc1b3f200163cac9
Nikhil Bhagat <knewboy.nykhil@gmail.com>
2022-11-08 14:45:33 +0545

    Very First Todo app Uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { clear } from '@testing-library/user-event/dist/clear'
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+
+
+
+const[todos, setTodos]= useState([])
+const[userInput, setUserInput]= useState("")
+
+const addData = () => {
+  setTodos([...todos, userInput])
 }
 
-export default App;
+
+return (
+    <div>
+      <h1>ToDos App</h1>
+      <input onChange={(event)=> setUserInput(event.target.value)} type="text" />
+      <button onClick={(event)  => addData() }>Add</button>
+
+      <ol>
+        {
+          todos.map((todo)=> <li>{todo}</li>)
+        }
+      </ol>
+    </div>
+  )
+
+}
+
+export default App
